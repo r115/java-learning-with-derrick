3b73b5fdba02d7ca95b07df873ea7195b5133c3c
Bernard Nandwa <bernard@nandwa.com>
2022-04-20 13:06:53 +0300

    Hello world code with some comments

--- src/com/andietech/Main.java
+++ src/com/andietech/Main.java
@@ -1,8 +1,23 @@
 package com.andietech;
 
+/**
+ * Declared a public class
+ *
+ * public: visibility modifier
+ * class: Java keyword
+ */
 public class Main {
-
+    /**
+     * Multiline comment
+     *
+     * String: Java data type
+     * args: function parameter. Data that we are passing to a function. Every parameter needs a type
+     * public: visibility modified. e.g. private, public and protected
+     * main(): entrypoint to a java program.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, World");
     }
 }
